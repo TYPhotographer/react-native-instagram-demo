fc20e845c27b2ef4b3ebadb1bbf78da5b7a080e0
陳泰延 <sam001425@gmail.com>
2021-11-01 16:08:23 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+// config source:
+// https://github.com/facebook/react-native/blob/master/packages/eslint-config-react-native-community/index.js
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  rules: {
+    semi: 'off',
+  },
+}
---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  tabWidth: 2,
+  singleQuote: true,
+  printWidth: 100,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  semi: false,
+}
